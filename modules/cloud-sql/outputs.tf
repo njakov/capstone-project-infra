c70f3a620eb95cf8ab380394909d1ec2ff859bbc
nina.jakovljevic <nina.jakovljevic@macys.com>
2025-11-19 15:13:27 +0100

    remove gsm

--- modules/cloud-sql/outputs.tf
+++ modules/cloud-sql/outputs.tf
@@ -15,11 +15,6 @@ output "db_user" {
   value       = google_sql_user.user.name
 }
 
-output "secret_id" {
-  description = "The simple name of the secret (e.g., 'petclinic-db-dev-password')"
-  value       = google_secret_manager_secret.db_password_secret.secret_id
-}
-
 output "private_ip_address" {
   description = "The private IP address of the Cloud SQL instance"
   value       = google_sql_database_instance.main.private_ip_address
