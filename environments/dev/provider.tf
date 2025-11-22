6724d9ce8a6d5406ccd5597885fe7eaf9a2654d1
nina.jakovljevic <nina.jakovljevic@macys.com>
2025-11-22 14:22:44 +0100

    fix

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -26,7 +26,8 @@ data "google_client_config" "default" {}
 
 # REQUIRED for Requirement #2 ("Use Helm")
 provider "helm" {
-  kubernetes {
+  # CHANGE: Add '=' here
+  kubernetes = {
     host                   = "https://${module.gke.cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
