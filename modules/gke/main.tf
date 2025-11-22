1521b005f91bcde7ffbdf9427b3bd4ee0e94877a
nina.jakovljevic <nina.jakovljevic@macys.com>
2025-11-22 18:52:15 +0100

    format all

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -122,7 +122,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     machine_type = "e2-medium" # Good, cost-effective default
     # Use the secure Container-Optimized OS (COS) image
-    image_type = "COS_CONTAINERD"
+    image_type   = "COS_CONTAINERD"
     disk_size_gb = 50
 
     # Use the dedicated SA you created
